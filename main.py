3d8757c921038372d38073196f6416c41811566d
umar pirmahomed <umarpirmahomed786@gmail.com>
2021-05-28 19:45:12 +0100

    Python basic variables

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
 
 print("hello world")
+var = 2 - 1
+var1 = 23 % 2
+print (var)
+print (var1)
+print(type(var))
+my_income = 100
+my_taxrate = 0.1
+my_taxes = my_taxrate * my_income
+print(my_taxes)
 
 
