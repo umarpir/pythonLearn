8890a0e258c21713236d37836b6147dc8d44a561
umar pirmahomed <umarpirmahomed786@gmail.com>
2021-06-07 18:18:13 +0100

    Python basic strings

--- strings.py
+++ strings.py
@@ -16,3 +16,11 @@ print(changestring * 10)
 x = 'hello world'
 print(x.upper())
 print(x.split())
+print('this is a string {}'.format('inserted'))
+print('the {0} {2} {1}' .format('quick', 'fox', 'brown'))
+print('the {f} {b} {f}' .format(q='quick', f='fox', b='brown'))
+result = 100/777
+print('the result was{r}' .format(r=result))
+print('the result was{r:6.3}' .format(r=result))
+name = 'jose'
+print(f'his name is {name}')
