eb0b643fde16d834f1b3c16b67d9c106b9d0f014
umar pirmahomed <umarpirmahomed786@gmail.com>
2021-06-07 17:55:07 +0100

    Python basic strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+mystring = "hello world"
+print('hello \nworld')
+print('hello \tworld')
+print(len(mystring))
+print(mystring[1])
+print(mystring[-1])
+print(mystring[1:])
+print(mystring[:3])
+print(mystring[1:3])
+print(mystring[::2])
+print(mystring[::-1])
+changestring = mystring[6:]
+print(changestring)
+changestring = 'bye ' + changestring
+print(changestring * 10)
+x = 'hello world'
+print(x.upper())
+print(x.split())
